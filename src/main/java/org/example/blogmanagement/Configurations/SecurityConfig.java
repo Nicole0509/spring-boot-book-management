7e1d06f3bcee1fa3f636495db846c1a6e2f1a9a4
Nicole0509 <niashavril0509@gmail.com>
2025-11-09 22:48:58 +0200

    Managed to access credentials by using users' emails from the DB and their corresponding passwords

--- src/main/java/org/example/blogmanagement/Configurations/SecurityConfig.java
+++ src/main/java/org/example/blogmanagement/Configurations/SecurityConfig.java
@@ -1,15 +1,23 @@
 package org.example.blogmanagement.Configurations;
 
+import org.example.blogmanagement.Services.UserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailService userDetailService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception {
@@ -19,5 +27,14 @@ public class SecurityConfig {
                 .csrf(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailService);
+
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return provider;
     }
 }
